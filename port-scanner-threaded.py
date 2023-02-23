ac701be27033f961c63d252b8887be83e94e44f4
kali 4 <nfornecidokali@gmail.com.br>
2023-02-23 07:27:50 -0500

    .

--- port-scanner-threaded.py
+++ port-scanner-threaded.py
@@ -1,74 +1,44 @@
-import argparse
-import socket
-import threading
+# nmap-scanner-threaded.py
+# Purpose: Python wrapper for nmap scanner
+# Author: Rayan Araujo
+# ##################
+# Version 0.1
+#   Initial build
 
 #clear terminal
 import os
 os.system('clear')
 
-def connection_scan(target_ip, target_port):
-    # Attempts to create a socket connection with the given IP and Port.
-    # If successful, the port is open. If not, the port is closed.
-    conn_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-    try:
-        conn_socket.connect((target_ip, target_port))
-        conn_socket.send(b'Banner_query\r\n')
-        print("     [+] {}/tcp open".format(target_port))
-
-    except OSError as e: # [!] kali linux is not catching the error
-        print("     [-] {}/tcp closed".format(target_port))
-
-    finally:
-        conn_socket.close() # Ensures the connection is closed
-
-
-def port_scan(target, port_num):
-    # Scan the ports indicated
-    # It first attempts to solve the IP, then enumerate through the ports
-    try:
-        target_ip = socket.gethostbyname(target)
-    except OSError:
-        print("[^] Cannot resolve {}: Unknown host".format(target))
-        return # Exit scan if IP is not resolved
-
-    try:
-        target_name = socket.gethostbyaddr(target_ip)
-    except OSError:
-        return
-        #print('[*] Scan Results for: {}'.format(target_ip))
-
-    t = threading.Thread(target=connection_scan, args=(target, int(port_num)))
-    t.start()
-
-
-
-
-
+import argparse
+import threading
+import nmap
 
 def argument_parser():
     # Allow user to specify target host and port.
-    parser = argparse.ArgumentParser(description="TCP port scanner. Accepts a hostname/IP address and list of ports to scan. Attempts to identify the service running on a port.")
-
+    parser = argparse.ArgumentParser(description="Nmap port scanner. Accepts hostname/IP address and list of ports to"
+    "to scan. Attempts to identify the service running on a port")
     parser.add_argument("-o", "--host", nargs="?", help="Host IP address")
     parser.add_argument("-p", "--ports", nargs="?", help="Comma-separated port list, such as '25,80,8080'")
-
     var_args = vars(parser.parse_args())
-
     return var_args
 
-if __name__ == '__main__':
+
+def nmap_scan(host_id, port_num):
+    # Use nmap to check status of host posts
+    nmap_scan = nmap.PortScanner()
+    nmap_scan.scan(host_id, port_num)
+    state = nmap_scan[host_id]['tcp'][int(port_num)]['state'] # Indicate the type of scan and port nmber.
+    result = ("[*]{host} tcp/{port} {satte}".format(host=host_id, port=port_num, state=state))
+
+    return result
+
+
+if __name__ == "__main__":
     try:
         user_args = argument_parser()
-        host_list = user_args["host"].split(",")
-        port_list = user_args["ports"].split(",") 
-
-        #search the list of ports in each host in the list of hosts
-        for host in host_list:
-            #print('\nScan Results for: {}'.format(host))
-            print('[*]\nScan Results for: {}'.format(host))
-            #port scan in the host
-            for port in port_list:
-                port_scan(host, port)
-
+        host = user_args["host"]
+        ports = user_args["ports"].split(",") # Make list from given port numbers "1,2,3,4,5"
+        for port in ports:
+            print(nmap_scan(host, port))
     except AttributeError:
-        print("Please provide the command-line arguments before running.")
+        print("Error. Please provide the command-line arguments before running")
